709b9da9f8d7ac25833a6e992a3bd513a155aaea
Kavishka <kavishkam1@gmail.com>
2021-04-24 17:18:14 +0530

    AF Project Second uploading

--- components/LandingPage/AppBar/AppBarComponent.jsx
+++ components/LandingPage/AppBar/AppBarComponent.jsx
@@ -1,43 +1,49 @@
 import React from 'react';
-import AppBar from "@material-ui/core/AppBar";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import FormControl from "@material-ui/core/FormControl";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import Toolbar from "@material-ui/core/Toolbar";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
-import Grid from "@material-ui/core/Grid"
+import Form from "react-bootstrap/Form";
+
+const brandStyle = {
+    color: "#5E4FA2",
+    fontWeight: 700
+}
+
+const linkStyle = {
+    color: "#5E4FA2",
+    fontFamily: 'Montserrat',
+    fontWeight: 500
+}
+
+const buttonStyle = {
+    color: "#5E4FA2",
+    backgroundColor:"#FEC949",
+    fontWeight: 600,
+    fontSize: "15px",
+    fontFamily: 'Montserrat'
+}
+
 function AppBarComponent(){
     return(
-        <div>
-        <AppBar position="fixed" color="transparent">
-            <Toolbar>
-                <Typography variant="h6">
-                    WebName
-                </Typography>
-                <Grid container justify="flex-end">
-                    <Button variant="h6">
-                        Home
-                    </Button>
-                    <Button variant="h6">
-                        Keynotes
-                    </Button>
-                    <Button variant="h6">
-                        Workshops
-                    </Button>
-                    <Button variant="h6">
-                        Committie
-                    </Button>
-                    <Button variant="h6">
-                        Past Proceeding
-                    </Button>
-                    <Button variant="outlined">
-                        Contact
-                    </Button>
-                </Grid>
-
-            </Toolbar>
-        </AppBar>
-        </div>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home" style={brandStyle}>WebName</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mx-auto">
+                        <Nav.Link href="#home" style={linkStyle} className="mx-3">Home</Nav.Link>
+                        <Nav.Link href="#link" style={linkStyle} className="mx-3">Keynotes</Nav.Link>
+                        <Nav.Link href="#link" style={linkStyle} className="mx-3">Workshops</Nav.Link>
+                        <Nav.Link href="#link" style={linkStyle} className="mx-3">Committie</Nav.Link>
+                        <Nav.Link href="#link" style={linkStyle} className="mx-3">Past Proceeding</Nav.Link>
+                    </Nav>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-success" style={buttonStyle}>Contact</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
     );
 }
 
--- index.jsx
+++ index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/LandingPage/App'
+import App from './components/LandingPage/App';
 
 ReactDOM.render(<App />, document.getElementById("root"));
